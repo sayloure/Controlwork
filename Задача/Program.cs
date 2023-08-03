307988b6a3c9cd2434ee4c023a48d108844bbad0
sayloure <135940846+sayloure@users.noreply.github.com>
2023-08-03 22:53:32 +0300

    Написан код для решения поставленной задачи на языке c#

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вход: массив строк inputArray
+Console.Write("Введите массив через пробел:");
+string[] inputArray = Console.ReadLine().Split(' ');
+// string[] inputArray = { "apple", "banana", "cat", "dog", "elephant" };
+
+// Создаем пустой массив outputArray
+string[] outputArray = new string[0];
+
+// Цикл for по строкам
+for (int i = 0; i < inputArray.Length; i++)
+{
+    string str = inputArray[i];
+
+    // Проверка длины строки
+    if (str.Length <= 3)
+    {
+        // Добавление строки в outputArray
+        Array.Resize(ref outputArray, outputArray.Length + 1);
+        outputArray[outputArray.Length - 1] = str;
+    }
+}
+
+// Вывести outputArray
+Console.WriteLine("Массив строк, длина которых меньше или равна 3 символам:");
+for (int i = 0; i < outputArray.Length; i++)
+{
+    Console.Write(outputArray[i]);
+    if (i < outputArray.Length -1)
+    {
+        
+        Console.Write(", ");
+    }
+    
+}
